699514fd236b1cb23bec40d5a2d024e399472d12
Ayush Verma <ayush02av@gmail.com>
2021-07-11 12:18:45 +0530

    update

--- backend/settings.py
+++ backend/settings.py
@@ -58,7 +58,7 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'frontend/templates')
+            os.path.join(BASE_DIR, 'frontend/frontend')
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -124,9 +124,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'frontend/static')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'frontend/frontend/static')
+]
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'frontend/media')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'frontend/frontend/media')
 
 django_heroku.settings(locals())

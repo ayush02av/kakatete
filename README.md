6b2af1546aea8360cd4d98c44c65845f42f14d91
Ayush Verma <ayush02av@gmail.com>
2021-07-11 11:58:57 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,12 @@
+# KAKATETE - online game hub
+
+[Link for project website : https://kakatete.herokuapp.com/](https://kakatete.herokuapp.com "KakaTete")
+
+# Stack used
+1. Django for backend
+2. HTML, CSS, JS for frontend
+
+# A small note
+1. A hobby project
+2. By students of ADGITM college
+3. It is under development
---
+++ database/models.py
@@ -0,0 +1,27 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+import django.contrib.auth.validators
+import os, glob, pytz, datetime, uuid
+
+UTC = pytz.utc
+datetimeNow = datetime.datetime.now(UTC)+datetime.timedelta(hours=5.5)
+
+class User(AbstractUser):
+	username = models.CharField(primary_key=True, error_messages={'unique': 'A user with that username already exists.'}, help_text='Required. 150 characters or fewer. Letters, digits and @/./+/-/_ only.', max_length=150, unique=True, validators=[django.contrib.auth.validators.UnicodeUsernameValidator()], verbose_name='username')
+	userid = models.UUIDField(unique=True, default=uuid.uuid4, editable=False)
+
+class Game(models.Model):
+	Game_Title = models.CharField(max_length=50, primary_key=True)
+	Max_Players = models.IntegerField(default=2)
+	Min_Players = models.IntegerField(default=2)
+
+	def __str__(self):
+		return self.Game_Title
+
+class GameServer(models.Model):
+	Server_Id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+	Game_Connected = models.ForeignKey(Game, on_delete=models.CASCADE)
+	Players_Connected = models.ManyToManyField(User)
+
+	def __str__(self):
+		return self.Server_Id.__str__()

c8b965bcbf9a0e364b56ae3844077e5436fbd234
Ayush Verma <ayush02av@gmail.com>
2021-08-12 15:19:38 +0530

    Update

--- frontend/views.py
+++ frontend/views.py
@@ -2,35 +2,14 @@ from django.shortcuts import render, redirect
 from database.models import *
 
 def Index(request):
-	return render(request, 'mainpage.html')
+	return render(request, 'index.html')
 
 def Play(request):
-	if request.method == 'GET':
-		if 'game' in request.GET:
-			try:
-				game = Game.objects.get(Game_Title=request.GET['game'])
-			except:
-				pass
-			else:
-				game = game.__str__().lower().replace(" ", "")
-				return render(request, 'index.html', {'game':game})
-	return redirect(Index)
+	return render(request, 'play.html')
 
-def About(request):
-	return render(request, 'about.html')
-
-def Testing(request):
-	return render(request, 'testing.html')
-
-def Chess(request):
-	return render(request, 'chess.html')
-
-def TicTacToe(request):
-	return render(request, 'tictactoe.html')
-
-def Ludo(request):
-	return render(request, 'ludo.html')
-
-def TestingPlay(request, game):
+def PlayGame(request, game):
 	game = game.__str__().lower().replace(" ", "")
 	return render(request, f'{game}.html')
+	
+def About(request):
+	return render(request, 'about.html')

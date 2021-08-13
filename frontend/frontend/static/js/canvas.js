da2cfc5bc91fe136ec488f93a6c629a6a8e11700
Amrit Thakkar <amritthakkar24@gmail.com>
2021-08-13 21:26:09 +0530

    Update

--- frontend/frontend/static/js/canvas.js
+++ frontend/frontend/static/js/canvas.js
@@ -1,5 +1,5 @@
 let canvas = document.getElementById("canvas");
-canvas.width = window.innerWidth - 60;
+canvas.width = window.innerWidth - 550;
 canvas.height = window.innerHeight * 0.6;
 let context = canvas.getContext("2d");
 context.fillStyle = "white";
